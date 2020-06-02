71211e60a07ae3a30102bffae4a8eae7268982fe
Peet Janse van Vuuren <peet@whereismytransport.com>
2020-06-02 10:41:52 +0200

    the react map gl draw thing

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef } from "react";
+import MapGL from "react-map-gl";
+import "./App.css";
+import "mapbox-gl/dist/mapbox-gl.css";
+import { Editor, DrawLineStringMode } from "react-map-gl-draw";
 
-function App() {
+const MAPBOX_TOKEN =
+  "pk.eyJ1IjoidWJlcmRhdGEiLCJhIjoiY2pwY3owbGFxMDVwNTNxcXdwMms2OWtzbiJ9.1PPVl0VLUQgqrosrI2nUhg";
+
+const App = () => {
+  const mapRef = useRef(null);
+
+  const [viewport, setViewport] = useState({
+    latitude: 37.78,
+    longitude: -122.41,
+    zoom: 11,
+  });
+
+  const [features, setFeatures] = useState(null);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MapGL
+        ref={mapRef}
+        width={"100%"}
+        height={"800px"}
+        mapboxApiAccessToken={MAPBOX_TOKEN}
+        latitude={viewport.latitude}
+        longitude={viewport.longitude}
+        zoom={viewport.zoom}
+        onViewportChange={(vp) => setViewport(vp)}
+      >
+        <Editor
+          features={features}
+          onUpdate={(onUpdateProps) => setFeatures(onUpdateProps.data)}
+          mode={new DrawLineStringMode()}
+        />
+      </MapGL>
     </div>
   );
-}
+};
 
 export default App;
